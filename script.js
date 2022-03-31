850ca5ea2b394db5caaf114a6e9441961b83325e
Marcus <marcusadrenaline@gmail.com>
2022-03-31 12:41:22 -0300

    commit 3

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-let lista;
+let listaclasses;
+let listacheck;
 let opcao_1;
 let opcao_2;
 let opcao_3;
 // pratos
 function mudarBordapratos(id) {
-    lista = document.getElementsByClassName('comida');
-    for (let i = 0; i < lista.length; i++) {
-        lista[i].style.border = "unset";
-      }
+    listaclasses = document.getElementsByClassName('comida');
+    listacheck = document.getElementsByClassName('check');
+    for (let i = 0; i < listaclasses.length; i++) {
+        listaclasses[i].style.border = "unset";
+    }
     document.getElementById(id).style.border = "5px solid green";
+    
     opcao_1 = id;
     alert(id)
 }
 // bebidas
 function mudarBordabebidas(id) {
-    lista = document.getElementsByClassName('bebida');
-    for (let i = 0; i < lista.length; i++) {
-        lista[i].style.border = "unset";
+    listaclasses = document.getElementsByClassName('bebida');
+    for (let i = 0; i < listaclasses.length; i++) {
+        listaclasses[i].style.border = "unset";
+        //document.getElementById("check").style.display = "none";
       }
     document.getElementById(id).style.border = "5px solid green";
+    //document.getElementById("check").style.display = "block";
     opcao_2 = id;
     alert(id)
 }
 // sobremesas
 function mudarBordasobremesas(id) {
-    lista = document.getElementsByClassName('sobremesa');
-    for (let i = 0; i < lista.length; i++) {
-        lista[i].style.border = "unset";
+    listaclasses = document.getElementsByClassName('sobremesa');
+    for (let i = 0; i < listaclasses.length; i++) {
+        listaclasses[i].style.border = "unset";
+        //document.getElementById("check").style.display = "none";
       }
     document.getElementById(id).style.border = "5px solid green";
+    //document.getElementById("check").style.display = "block";
     opcao_3 = id;
     alert(id)
 }
