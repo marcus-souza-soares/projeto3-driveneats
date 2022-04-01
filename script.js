d18beaa275e4e8150e212464b573fd294d7022bb
Marcus <marcusadrenaline@gmail.com>
2022-04-01 09:30:22 -0300

    commit

--- script.js
+++ script.js
@@ -1,48 +1,75 @@
-let listaclasses;
-let listacheck;
-let opcao_1;
-let opcao_2;
-let opcao_3;
-// pratos
-function mudarBordapratos(id) {
-    listaclasses = document.getElementsByClassName('comida');
-    listacheck = document.getElementsByClassName('check');
-    for (let i = 0; i < listaclasses.length; i++) {
-        listaclasses[i].style.border = "unset";
+let preco_prato;
+let preco_bebida;
+let preco_sobremesa;
+let total;
+
+
+function mudarBordaprato(elemento){
+    const verde = document.querySelector(".bordaverdeprato");
+    const aparece = document.querySelector(".displaymostrarprato");
+    const check = document.querySelector( "#" + elemento.id + " > ion-icon")
+    if (verde !== null){
+        verde.classList.remove("bordaverdeprato");
+        aparece.classList.remove("displaymostrarprato");
     }
-    document.getElementById(id).style.border = "5px solid green";
-    
-    opcao_1 = id;
-    alert(id)
+    elemento.classList.add("bordaverdeprato");
+    check.classList.add("displaymostrarprato");
+    let pedido = elemento.id;
+    preco_prato = document.querySelector("#" + elemento.id + "  .precoprato").innerHTML
+    preco_prato = parseFloat(preco_prato)
+    alert(pedido);
+    fecharpedidoVerde();
 }
-// bebidas
-function mudarBordabebidas(id) {
-    listaclasses = document.getElementsByClassName('bebida');
-    for (let i = 0; i < listaclasses.length; i++) {
-        listaclasses[i].style.border = "unset";
-        //document.getElementById("check").style.display = "none";
-      }
-    document.getElementById(id).style.border = "5px solid green";
-    //document.getElementById("check").style.display = "block";
-    opcao_2 = id;
-    alert(id)
+
+function mudarBordabebida(elemento){
+    const verde = document.querySelector(".bordaverdebebida");
+    const aparece = document.querySelector(".displaymostrarbebida");
+    const check = document.querySelector( "#" + elemento.id + " > ion-icon")
+    if (verde !== null){
+        verde.classList.remove("bordaverdebebida");
+        aparece.classList.remove("displaymostrarbebida");
+    }
+    elemento.classList.add("bordaverdebebida");
+    check.classList.add("displaymostrarbebida");
+    preco_bebida = document.querySelector("#" + elemento.id + " .precobebida").innerHTML
+    preco_bebida = parseFloat(preco_bebida)
+    let pedido = elemento.id;
+    alert(pedido);
+    fecharpedidoVerde();
 }
-// sobremesas
-function mudarBordasobremesas(id) {
-    listaclasses = document.getElementsByClassName('sobremesa');
-    for (let i = 0; i < listaclasses.length; i++) {
-        listaclasses[i].style.border = "unset";
-        //document.getElementById("check").style.display = "none";
-      }
-    document.getElementById(id).style.border = "5px solid green";
-    //document.getElementById("check").style.display = "block";
-    opcao_3 = id;
-    alert(id)
+
+function mudarBordasobremesa(elemento){
+    const verde = document.querySelector(".bordaverdesobremesa");
+    const aparece = document.querySelector(".displaymostrarsobremesa");
+    const check = document.querySelector( "#" + elemento.id + " > ion-icon")
+    if (verde !== null){
+        verde.classList.remove("bordaverdesobremesa");
+        aparece.classList.remove("displaymostrarsobremesa");
+    }
+    elemento.classList.add("bordaverdesobremesa");
+    check.classList.add("displaymostrarsobremesa");
+    preco_sobremesa = document.querySelector("#" + elemento.id + " .precosobremesa").innerHTML
+    preco_sobremesa = parseFloat(preco_sobremesa)
+    let pedido = elemento.id;
+    alert(pedido);
+    fecharpedidoVerde();
 }
 
 
+total = preco_bebida + preco_prato + preco_sobremesa;
 
+function fecharpedidoVerde() {
+    if ((typeof preco_prato === 'number') && (typeof preco_bebida === 'number') && (typeof preco_sobremesa === 'number')){
+        document.querySelector(".fecha-pedido").style.background = "green";
+    }    
+}
 // botao de pedido
 function fecharpedido() {
-    alert("ainda vai funcionar")
+    if ((typeof preco_prato === 'number') && (typeof preco_bebida === 'number') && (typeof preco_sobremesa === 'number')){
+        alert("vamos seguir");
+        document.querySelector(".fecha-pedido").style.background = "green";
+    } else {
+        alert("o pedido ainda nao esta pronto");
+    }
 }
+
